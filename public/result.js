bd11c48ac9c5c1191184dd6ddbf9f833a942f21d
Abdullah1B <a.j.baskran@gmail.com>
2021-12-15 21:33:16 +0300

    completed

--- public/result.js
+++ public/result.js
@@ -1,15 +1,24 @@
-document.getElementById('form-submit').addEventListener('submit' , post);
+document.getElementById('form-submit').addEventListener('submit', post);
 
-function post(e){
-    e.preventDefault();    
-    var text = document.getElementById('input-text').value;
-    var prams = "text="+text;
+function post(e) {
+    e.preventDefault();
+    var text = document.getElementById('Text').value;
+    var prams = "text=" + text;
     var request = new XMLHttpRequest();
-    request.open('POST','./fasttext',true);
+    request.open('POST', './fasttext', true);
     request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    request.onload = function(){
-        var classftion = JSON.parse(this.responseText)
-        console.log(classftion.data);
+    request.onload = function () {
+        var classification = JSON.parse(this.responseText)
+        // console.log(classftion.data);
+        if (classification.data.length > 3){
+            var label = classification.data.substring(9, classification.data.length); 
+            var result = "<div class = 'card'> <div class='inner_card'>" + text + "</div> <div class = 'right_side'>" +"LABEL:"+ label+ "</div></div>"
+            document.getElementById("result_card").innerHTML +=result;
+        }else{
+            var result = "<div class = 'card'> <div class='inner_card'>" + "Empty text" + "</div> <div class = 'right_side'>" +"LABEL:"+ classification.data+ "</div></div>"
+            document.getElementById("result_card").innerHTML +=result;
+        }
+
     }
     request.send(prams);
 
